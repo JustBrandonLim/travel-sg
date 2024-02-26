a95ce483a68be34921ed377594097d029df237f9
Brandon Lim Jing Zhi <brandonlimjingzhi@gmail.com>
2024-02-26 15:39:29 +0800

    testing deployment.

--- src/app/api/sentiment-analysis/route.ts
+++ src/app/api/sentiment-analysis/route.ts
@@ -2,9 +2,8 @@ import { pipeline } from "@xenova/transformers";
 
 export async function POST(request: Request) {
   const data = await request.json();
-
-  const pipe = await pipeline("sentiment-analysis");
-  const out = await pipe(data);
+  const analyse = await pipeline("sentiment-analysis");
+  const out = await analyse(data);
 
   return Response.json(out[0]);
 }
