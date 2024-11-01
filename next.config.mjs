3e2e1cc11a30ddd07b1a6fde47f111ec50a5ffbc
Brandon Lim Jing Zhi <brandonlimjingzhi@gmail.com>
2024-11-02 03:10:52 +0800

    test huggingface

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,19 @@
+import path from "path";
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url); // get the resolved path to the file
+const __dirname = path.dirname(__filename); // get the name of the directory
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     serverComponentsExternalPackages: ["sharp", "onnxruntime-node"],
   },
+  webpack(config, { isServer }) {
+	  config.resolve.alias['@huggingface/transformers'] = path.resolve(__dirname, 'node_modules/@huggingface/transformers');
+	
+    return config;
+	}
 };
 
 export default nextConfig;
