14141c157064fcdc51f33a84d4d656f7eabcf575
Brandon Lim Jing Zhi <brandonlimjingzhi@gmail.com>
2024-11-02 03:14:13 +0800

    test huggingface

--- next.config.mjs
+++ next.config.mjs
@@ -7,11 +7,11 @@ const __dirname = path.dirname(__filename); // get the name of the directory
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack(config, { isServer }) {
-   /*config.resolve.alias = {
+    config.resolve.alias = {
             ...config.resolve.alias,
             "sharp$": false,
             "onnxruntime-node$": false,
-    }*/
+    }
     
 	  config.resolve.alias['@huggingface/transformers'] = path.resolve(__dirname, 'node_modules/@huggingface/transformers');
 	
