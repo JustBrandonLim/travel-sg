d7c4f71869369915054381eec52aa9c041ff08b4
Brandon Lim Jing Zhi <brandonlimjingzhi@gmail.com>
2024-11-02 03:23:06 +0800

    test

--- next.config.mjs
+++ next.config.mjs
@@ -8,9 +8,10 @@ const __dirname = path.dirname(__filename); // get the name of the directory
 const nextConfig = {
   webpack(config, { isServer }) {
     config.resolve.alias = {
-            ...config.resolve.alias,
-            "sharp$": false,
-            "onnxruntime-node$": false,
+      ...config.resolve.alias,
+      "sharp$": false,
+      "onnxruntime-node$": false,
+      "@huggingface/transformers$": false
     }
     
 	  config.resolve.alias['@huggingface/transformers'] = path.resolve(__dirname, 'node_modules/@huggingface/transformers');
