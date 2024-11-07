b4d236d7b1fe7f1462cd4498ae2d8c66b5d6034f
Brandon Lim Jing Zhi <brandonlimjingzhi@gmail.com>
2024-11-07 17:55:45 +0800

    add edge runtime

--- src/app/api/bus/update/route.ts
+++ src/app/api/bus/update/route.ts
@@ -3,6 +3,7 @@ import { GetBusServices, GetBusStops, GetBusRoutes } from "@services/lta-datamal
 import { InsertBusStops, InsertBusServices, InsertBusRoutes } from "@services/travel-sg";
 import { NextResponse } from "next/server";
 
+export const runtime = "edge";
 export const revalidate = 0;
 
 export async function GET() {
