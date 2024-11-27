0ed53e3a24e34c61b187f0809e7634a4f8877674
JustBrandonLim <brandonlimjingzhi@gmail.com>
2024-11-27 16:06:11 +0800

    test

--- next.config.mjs
+++ next.config.mjs
@@ -6,12 +6,12 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const nextConfig = {
+  experimental: {
+    serverComponentsExternalPackages: ["@huggingface/transformers", "sharp", "onnxruntime-node"],
+  },
   webpack: (config) => {
     config.resolve.alias["@huggingface/transformers"] = path.resolve(__dirname, "node_modules/@huggingface/transformers");
     return config;
-  },
-  experimental: {
-    serverComponentsExternalPackages: ["@huggingface/transformers"],
   },
 };
 
