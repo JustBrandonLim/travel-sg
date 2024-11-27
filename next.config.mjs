908ccd13ff588d0015809a0d336e1c5225a7f994
JustBrandonLim <brandonlimjingzhi@gmail.com>
2024-11-27 15:59:06 +0800

    test

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,14 @@
 /** @type {import('next').NextConfig} */
+import path from "path";
+import { fileURLToPath } from "url";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+
 const nextConfig = {
+  webpack: {
+    config.resolve.alias['@huggingface/transformers'] = path.resolve(__dirname, 'node_modules/@huggingface/transformers');
+  },
   experimental: {
     serverComponentsExternalPackages: ["@huggingface/transformers"],
   },
