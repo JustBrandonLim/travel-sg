6ad3f7c2ec260c824d4c1f6e56cc6a43e69b9a1f
JustBrandonLim <brandonlimjingzhi@gmail.com>
2024-11-27 16:54:00 +0800

    test exclude output

--- next.config.mjs
+++ next.config.mjs
@@ -6,9 +6,11 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const nextConfig = {
-  output: "standalone",
   experimental: {
     serverComponentsExternalPackages: ["sharp", "onnxruntime-node"],
+    outputFileTracingExcludes: {
+      "*": ["node_modules/onnxruntime-node/bin"],
+    },
   },
   webpack: (config) => {
     config.resolve.alias["@huggingface/transformers"] = path.resolve(__dirname, "node_modules/@huggingface/transformers");
