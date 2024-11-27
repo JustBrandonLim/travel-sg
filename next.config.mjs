96da5511709ae6247297f224aaaf7f08560c0583
JustBrandonLim <brandonlimjingzhi@gmail.com>
2024-11-27 16:51:12 +0800

    test output mode

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,7 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 const nextConfig = {
+  output: "standalone",
   experimental: {
     serverComponentsExternalPackages: ["sharp", "onnxruntime-node"],
   },
